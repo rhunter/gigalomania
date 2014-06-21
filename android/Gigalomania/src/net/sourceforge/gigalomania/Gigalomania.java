3371fc8eb2cbb04f21936407bfd4cab9cd6fe76c
Mark Harman <mark.harman@ntlworld.com>
2014-06-21 19:40:37 +0100

    Keep screen awake on Android.

--- android/Gigalomania/src/net/sourceforge/gigalomania/Gigalomania.java
+++ android/Gigalomania/src/net/sourceforge/gigalomania/Gigalomania.java
@@ -4,11 +4,13 @@ import org.libsdl.app.SDLActivity;
 
 import android.content.Intent;
 import android.net.Uri;
+import android.os.Bundle;
 import android.util.Log;
+import android.view.WindowManager;
 
 /* 
  * A sample wrapper class that just calls SDLActivity 
- */ 
+ */
 
 public class Gigalomania extends SDLActivity {
     private static final String TAG = "Gigalomania";
@@ -18,4 +20,13 @@ public class Gigalomania extends SDLActivity {
         Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
     	startActivity(browserIntent);
 	}
+
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        
+        // keep screen active - see http://stackoverflow.com/questions/2131948/force-screen-on
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+    }
 }
